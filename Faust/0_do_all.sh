cf68542fd2f08c4d1b8a3035e4ab94a3227dcc75
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-04-19 19:19:22 -0700

    Faust installer

---
+++ Faust/0_do_all.sh
@@ -0,0 +1,47 @@
+#! /usr/bin/env bash
+
+set -e
+
+echo "Setting Faust version"
+export FAUST_VERSION="2.79.3"
+export FAUST_URL="https://github.com/grame-cncm/faust/releases/download/${FAUST_VERSION}/faust-${FAUST_VERSION}.tar.gz"
+export FAUST_PATH="$HOME/Projects/faust-${FAUST_VERSION}"
+echo "${FAUST_PATH}"
+
+echo "Defining LOGFILE"
+mkdir --parents $PWD/Logs
+export LOGFILE=$PWD/Logs/install_faust.log
+rm --force $LOGFILE
+
+# https://github.com/grame-cncm/faust/wiki/Building
+echo "Installing build dependencies"
+export DEBIAN_FRONTEND="noninteractive"
+/usr/bin/time sudo apt-get install --assume-yes --no-install-recommends \
+  libmicrohttpd-dev \
+  libpolly-18-dev \
+  libzstd-dev \
+  llvm \
+  llvm-dev \
+  pkg-config \
+  >> $LOGFILE 2>&1
+
+mkdir --parents $FAUST_PATH
+rm --force --recursive $FAUST_PATH
+pushd $HOME/Projects
+  echo ""
+  echo "Downloading Faust source"
+  curl -sL "${FAUST_URL}" \
+    | tar xzf -
+popd
+
+pushd $FAUST_PATH
+  echo ""
+  echo "Building Faust"
+  /usr/bin/time make all \
+    >> $LOGFILE 2>&1
+  echo "Installing Faust"
+  sudo make install \
+    >> $LOGFILE 2>&1
+popd
+
+echo "Finished"
