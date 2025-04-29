883619b26f522f94081735b692b8804634eae5f6
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-04-29 13:42:23 -0700

    get rid of WebChuck for now - too hard to run locally

--- apt_pkg_db_updates.sh
+++ apt_pkg_db_updates.sh
@@ -2,18 +2,28 @@
 
 set -e
 
+echo "Defining LOGFILE"
+mkdir --parents $PWD/Logs
+export LOGFILE=$PWD/Logs/apt_pkg_db_updates.log
+rm --force $LOGFILE
+
 echo "Upgrading packages"
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-get update > /dev/null 2>&1
-sudo apt-get upgrade -y > /dev/null 2>&1
+sudo apt-get update \
+  >> $LOGFILE 2>&1
+sudo apt-get upgrade -y \
+  >> $LOGFILE 2>&1
 
 echo "Updating apt-file database"
-sudo apt-file update > /dev/null 2>&1
+sudo apt-file update \
+  >> $LOGFILE 2>&1
 
 echo "Updating locate database"
-sudo updatedb > /dev/null 2>&1
+sudo updatedb \
+  >> $LOGFILE 2>&1
 
 echo "Updating manual database"
-sudo mandb > /dev/null 2>&1
+sudo mandb \
+  >> $LOGFILE 2>&1
 
 echo "Finished"
