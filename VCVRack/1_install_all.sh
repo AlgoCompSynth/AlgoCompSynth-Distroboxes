c28782fd3f38c344b3412a37090b7ad2ce753d9d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-05-01 20:28:24 -0700

    VCVRack install tested

--- VCVRack/1_install_all.sh
+++ VCVRack/1_install_all.sh
@@ -5,5 +5,6 @@ set -e
 ./terminal_setup.sh
 ./apt_basic_devel.sh
 ./apt_audio_base.sh
+./linux_dependencies.sh
 ./install_vcvrack.sh
 ./apt_pkg_db_updates.sh
--- VCVRack/install_vcvrack.sh
+++ VCVRack/install_vcvrack.sh
@@ -2,25 +2,17 @@
 
 set -e
 
+echo ""
+echo "*** VCVRack ***"
+
 echo "Defining LOGFILE"
 mkdir --parents $PWD/Logs
 export LOGFILE=$PWD/Logs/install_vcvrack.log
 rm --force $LOGFILE
 
-echo "Installing build dependencies"
-export DEBIAN_FRONTEND=noninteractive
-/usr/bin/time sudo apt-get install -qqy --no-install-recommends \
-  jq \
-  libglew-dev \
-  libxcursor-dev \
-  libxi-dev \
-  libxinerama-dev \
-  libxrandr-dev \
-  >> $LOGFILE 2>&1
-
 mkdir --parents $HOME/Projects
+echo "Cloning repositories"
 pushd $HOME/Projects
-  echo "Cloning repositories"
   rm --force --recursive Rack VCVBook
   /usr/bin/time git clone \
     https://github.com/VCVRack/Rack.git \
@@ -28,10 +20,10 @@ pushd $HOME/Projects
   /usr/bin/time git clone \
     https://github.com/LOGUNIVPM/VCVBook.git \
     >> $LOGFILE 2>&1
-popd
+popd > /dev/null
 
+echo "Fetching Rack submodules"
 pushd $HOME/Projects/Rack
-  echo "Fetching Rack submodules"
   /usr/bin/time git submodule update --init --recursive \
     >> $LOGFILE 2>&1
 
@@ -43,15 +35,15 @@ pushd $HOME/Projects/Rack
   /usr/bin/time make --jobs=`nproc` \
     >> $LOGFILE 2>&1
 
-popd
+popd > /dev/null
 
 cp -rp $HOME/Projects/VCVBook/ABC $HOME/Projects/Rack/plugins
 
+echo "Building ABC plugin"
 pushd $HOME/Projects/Rack/plugins/ABC
-  echo "Building ABC plugin"
   /usr/bin/time make --jobs=`nproc` \
     >> $LOGFILE 2>&1
-popd
+popd > /dev/null
 
 echo "Installing 'Rack' script"
 cp Rack $HOME/.local/bin/
