d0256dce8abffccd0fa41d0373030c1d4dba30f9
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-05-03 13:48:29 -0700

    Fundamental modules installed

--- VCVRack/install_vcvrack.sh
+++ VCVRack/install_vcvrack.sh
@@ -13,21 +13,32 @@ rm --force $LOGFILE
 mkdir --parents $HOME/Projects
 echo "Cloning repositories"
 pushd $HOME/Projects
-  rm --force --recursive Rack VCVBook
-  /usr/bin/time git clone \
+  rm --force --recursive Rack Fundamental VCVBook 
+
+  /usr/bin/time git clone --branch v2.6.3 \
     https://github.com/VCVRack/Rack.git \
     >> $LOGFILE 2>&1
+  cd Rack
+    git submodule update --init --recursive \
+      >> $LOGFILE 2>&1
+  cd ..
+
+  /usr/bin/time git clone --branch v2.6.3 \
+    https://github.com/VCVRack/Fundamental.git \
+    >> $LOGFILE 2>&1
+  cd Fundamental
+    git submodule update --init --recursive \
+      >> $LOGFILE 2>&1
+  cd ..
+
   /usr/bin/time git clone \
     https://github.com/LOGUNIVPM/VCVBook.git \
     >> $LOGFILE 2>&1
+
 popd > /dev/null
 
-echo "Fetching Rack submodules"
+echo "make dep"
 pushd $HOME/Projects/Rack
-  /usr/bin/time git submodule update --init --recursive \
-    >> $LOGFILE 2>&1
-
-  echo "make dep"
   /usr/bin/time make dep \
     >> $LOGFILE 2>&1
 
@@ -37,9 +48,17 @@ pushd $HOME/Projects/Rack
 
 popd > /dev/null
 
-cp -rp $HOME/Projects/VCVBook/ABC $HOME/Projects/Rack/plugins
+echo "Building Fundamental plugin"
+cp -rp $HOME/Projects/Fundamental $HOME/Projects/Rack/plugins/
+pushd $HOME/Projects/Rack/plugins/Fundamental
+  /usr/bin/time make dep \
+    >> $LOGFILE 2>&1
+  /usr/bin/time make --jobs=`nproc` \
+    >> $LOGFILE 2>&1
+popd > /dev/null
 
-echo "Building ABC plugin"
+echo "Building VCVBook ABC plugin"
+cp -rp $HOME/Projects/VCVBook/ABC $HOME/Projects/Rack/plugins/
 pushd $HOME/Projects/Rack/plugins/ABC
   /usr/bin/time make --jobs=`nproc` \
     >> $LOGFILE 2>&1
